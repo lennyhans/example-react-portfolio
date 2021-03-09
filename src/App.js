93d83baa9df605bf332a6d3bb5e058f57259192f
Lenny Andreu <landreuhs.29@gmail.com>
2021-03-08 22:56:56 -0300

    Add ArticleAbout data and reorder data of skills component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ParallaxImage from './components/ParallaxImage';
 import HighlightItem from './components/HighlightItem';
 import HighlightContainer from './components/HighlightContainer';
 import ArticleContainer from './components/ArticleContainer';
+import ArticleAbout from './components/ArticleAbout';
 import SkillBar from './components/SkillBar';
 
 function App() {
@@ -34,15 +35,30 @@ function App() {
       manyMore : true
     }
   ];
-  
-  const highlightRendered = highlightItems.map( (el, i) =>{
-    return (<HighlightItem 
-      name={el.name} 
-      ammount={el.ammount} manyMore={el.manyMore} key={i}></HighlightItem>
-    );
-  });
 
-  const skillsRendered = [
+  // Read from somewhere
+  const aboutMeData = {
+    paragraphs : [ 
+      {
+        text : `We have created a fictional 'personal' website/blog, and our fictional character is a hobby photographer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+          qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+        showMobile : true
+      },
+      {
+        text : `Welcome to my website. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+        showMobile : false
+      }
+    ],
+    avatar : {
+      photo : "https://www.w3schools.com/w3images/avatar_hat.jpg",
+      caption : "My Name"
+    }
+  };
+
+  const skillsData = [
     {
         name : "Photography",
         icon : "fa-camera",
@@ -57,7 +73,16 @@ function App() {
       name : "GIMP",
       icon : "fa-photo",
       percentage : 75
-    }].map( (el, i) => {
+    }];
+  
+  const highlightRendered = highlightItems.map( (el, i) =>{
+    return (<HighlightItem 
+      name={el.name} 
+      ammount={el.ammount} manyMore={el.manyMore} key={i}></HighlightItem>
+    );
+  });
+
+  const skillsRendered = skillsData.map( (el, i) => {
     return (
       <SkillBar name={el.name} 
         icon={el.icon} 
@@ -65,7 +90,9 @@ function App() {
         key={i}></SkillBar>
     );
   });
-  
+
+  /** */
+
   return (
     <div className="App">
       <Navbar></Navbar>
@@ -74,8 +101,11 @@ function App() {
         title="ABOUT ME" 
         summary="I love photography" 
         id="about">
-            <p className="w3-large w3-center w3-padding-16">Im really good at:</p>
-            {skillsRendered}
+          <div className="w3-row">
+            <ArticleAbout {...aboutMeData}></ArticleAbout>
+          </div>
+          <p className="w3-large w3-center w3-padding-16">Im really good at:</p>
+          {skillsRendered}
         </ArticleContainer>
       <HighlightContainer>{highlightRendered}</HighlightContainer>
       <ParallaxImage text="PORTFOLIO" image="bgimg-2" id="portfolio"></ParallaxImage>
